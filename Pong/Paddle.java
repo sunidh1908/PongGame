3cd54b288af35807b49ad8264796ea6c244a9c83
sunidh1908 <99424874+sunidh1908@users.noreply.github.com>
2022-08-27 14:11:51 +0530

    Add files via upload

---
+++ Pong/Paddle.java
@@ -0,0 +1,7 @@
+public class Paddle {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.print(n);
+    }
+}
